52c989bab94c08571906161c34202afba748c0fc
alkp <apasenko@gmail.com>
2021-10-06 19:26:51 +0300

    Добавляем active_support и хелперы загрузки конфигураций

---
+++ 00.the_beginning_of_any_microservice/config/application_loader.rb
@@ -0,0 +1,26 @@
+module ApplicationLoader
+  extend self
+
+  def load_app!
+    init_app
+  end
+
+  private
+
+  def init_app
+    require_dir 'config/initializers'
+  end
+
+  def require_file(path)
+    require File.join(root, path)
+  end
+
+  def require_dir(path)
+    path = File.join(root, path)
+    Dir["#{path}/**/*rb"].each { |file| require file }
+  end
+
+  def root
+    File.expand_path('..', __dir__)
+  end
+end
