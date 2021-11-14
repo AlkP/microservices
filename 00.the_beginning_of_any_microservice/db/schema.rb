215e74f15059ae91b5246f15259c3892e01a9028
alkp <apasenko@gmail.com>
2021-11-14 14:30:36 +0300

    Сохраняем схему БД в ./db/schema.rb

---
+++ 00.the_beginning_of_any_microservice/db/schema.rb
@@ -0,0 +1,23 @@
+Sequel.migration do
+  change do
+    create_table(:ads, :ignore_index_errors=>true) do
+      primary_key :id, :type=>:Bignum
+      Bignum :user_id, :null=>false
+      String :title, :text=>true, :null=>false
+      String :description, :text=>true, :null=>false
+      String :city, :text=>true, :null=>false
+      Float :lat
+      Float :lon
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
+      
+      index [:user_id], :name=>:index_ads_on_user_id
+    end
+    
+    create_table(:schema_migrations) do
+      String :filename, :text=>true, :null=>false
+      
+      primary_key [:filename]
+    end
+  end
+end
